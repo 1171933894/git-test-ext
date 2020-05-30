051309bef3f09efa9444b7155c5ec31a3fe68f2a
heyuanxin3 <heyuanxin3@jd.com>
2020-05-30 18:03:52 +0800

    sit c 2

--- jdk-test/src/main/java/Test9.java
+++ jdk-test/src/main/java/Test9.java
@@ -9,5 +9,6 @@ public class Test9 {
         System.out.println ("hello world!");
         System.out.println ("hello world!");
         System.out.println ("hello world!");
+        System.out.println ("hello world!");
     }
 }
