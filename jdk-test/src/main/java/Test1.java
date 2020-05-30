d384147e89515561aa82b2f26f98cbeac1474899
heyuanxin3 <heyuanxin3@jd.com>
2020-05-30 15:29:48 +0800

    Revert "dev1 commit"
    This reverts commit 258dd03ae37bfc1e3e795dec07265208c24b32ac.

--- jdk-test/src/main/java/Test1.java
+++ jdk-test/src/main/java/Test1.java
@@ -7,6 +7,5 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println ("hello world!");
         System.out.println ("hello world!");
-        System.out.println ("hello world!");
     }
 }
--- jdk-test/src/main/java/Test3.java
+++ jdk-test/src/main/java/Test3.java
@@ -7,6 +7,5 @@ public class Test3 {
     public static void main(String[] args) {
         System.out.println ("hello world!");
         System.out.println ("hello world!");
-        System.out.println ("hello world!");
     }
 }
