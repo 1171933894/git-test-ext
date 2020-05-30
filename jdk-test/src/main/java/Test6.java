7328f6efc057b1a835d67b684fb9c35279bda825
heyuanxin3 <heyuanxin3@jd.com>
2020-05-30 18:07:15 +0800

    master c 2

--- jdk-test/src/main/java/Test6.java
+++ jdk-test/src/main/java/Test6.java
@@ -8,5 +8,6 @@ public class Test6 {
         System.out.println ("hello world!");
         System.out.println ("hello world!");
         System.out.println ("hello world!");
+        System.out.println ("hello world!");
     }
 }
