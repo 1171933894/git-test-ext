4e8da99c7fc1ad541f1370c551a0672203da429b
heyuanxin3 <heyuanxin3@jd.com>
2020-05-30 14:32:37 +0800

    first

---
+++ jdk-test/src/main/java/Test.java
@@ -0,0 +1,14 @@
+/**
+ * @Description:
+ * @Author: heyuanxin3
+ * @Date: 2020/5/30 7:57
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println ("123");
+        System.out.println ("234");
+        System.out.println ("345");
+        System.out.println ("456");
+        System.out.println ("567");
+    }
+}
---
+++ jdk-test/src/main/java/Test8.java
@@ -0,0 +1,11 @@
+/**
+ * @Description:
+ * @Author: heyuanxin3
+ * @Date: 2020/5/30 7:57
+ */
+public class Test8 {
+    public static void main(String[] args) {
+        System.out.println ("hello world!");
+        System.out.println ("hello world!");
+    }
+}
