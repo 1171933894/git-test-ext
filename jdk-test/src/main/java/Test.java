bed336bd2f0d0683e3d03102f0d8d65eed5c2d72
heyuanxin3 <heyuanxin3@jd.com>
2020-06-02 20:55:41 +0800

    test

---
+++ jdk-test/src/main/java/Test.java
@@ -0,0 +1,16 @@
+/**
+ * @Description:
+ * @Author: heyuanxin3
+ * @Date: 2020/5/30 7:57
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println ("123");
+        System.out.println ("234");
+        System.out.println ("345");
+        System.out.println ("456");
+        System.out.println ("567");
+        System.out.println ("sit");
+        System.out.println ("test");
+    }
+}
