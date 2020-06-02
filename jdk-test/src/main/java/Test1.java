21a4dd32ab8d00416108a5816932ac55fc3c47de
heyuanxin3 <heyuanxin3@jd.com>
2020-06-02 22:14:18 +0800

    test c 1

---
+++ jdk-test/src/main/java/Test1.java
@@ -0,0 +1,19 @@
+/**
+ * @Description:
+ * @Author: heyuanxin3
+ * @Date: 2020/5/30 7:57
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println ("123");
+        System.out.println ("234");
+        System.out.println ("345");
+        System.out.println ("456");
+        System.out.println ("567");
+        System.out.println ("sit");
+        System.out.println ("test");
+        System.out.println ("stash");
+        System.out.println ("stash1");
+        System.out.println ("stash2");
+    }
+}
